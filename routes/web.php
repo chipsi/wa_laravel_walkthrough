07dc45723152d57cec566e489a262feef917b0b1
Jiří Lýsek <jiri.lysek@mendelu.cz>
2017-02-23 09:36:22 +0100

    zruseni welcome stranky

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('person::list'));
 });
 
 Route::get('/osoby', 'PersonsList@show')->name('person::list');
